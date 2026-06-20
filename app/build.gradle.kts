a79afe8f8758a4706090813a78093208f1fc2a5c
RedBanMana <kyle.kornetzke@gmail.com>
2026-06-19 19:18:01 -0500

    update to app version 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ android {
         targetSdk = 36
         versionCode = 2
         versionName = "1.0.1"
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
